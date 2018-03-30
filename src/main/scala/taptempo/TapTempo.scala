0da5b488740e808061855dc72d5a4e7d96db7605
Fabien Marteau <fabien.marteau@armadeus.com>
2018-03-30 17:25:05 +0200

    starting up with tests

--- src/main/scala/taptempo/TapTempo.scala
+++ src/main/scala/taptempo/TapTempo.scala
@@ -4,7 +4,12 @@ import chisel3._
 
 class TapTempo extends Module {
   val io = IO(new Bundle {
+    val bpm = Output(UInt(9.W))
     val button = Input(Bool())
-    val bpm = Output(UInt(16.W))
   })
+
+  val bpm_o = Reg(UInt(9.W))
+
+  io.bpm := bpm_o
+  bpm_o := io.button
 }
---
+++ src/test/scala/taptempo/TapTempoUnitTest.scala
@@ -0,0 +1,28 @@
+package taptempo
+
+import chisel3.iotesters
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+
+class TapTempoUnitTester(t: TapTempo) extends PeekPokeTester(t) {
+  private val tptmp = t
+
+  for(i <- 1 to 100 by 1) {
+    poke(tptmp.io.button, 1)
+    step(1)
+//    expect(tptmp.io.bpm, 0)
+//    expect(tptmp.io.bpm, 1)
+    poke(tptmp.io.button, 0)
+    step(1)
+  }
+}
+
+
+class TapTempoTester extends ChiselFlatSpec {
+  behavior of "TapTempoTester"
+
+  it should "launch a simple test" in {
+    chisel3.iotesters.Driver(() => new TapTempo) {
+      c => new TapTempoUnitTester(c)
+    } should be(true)
+  }
+}
